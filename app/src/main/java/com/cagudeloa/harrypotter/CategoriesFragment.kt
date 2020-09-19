76688ea96bed290ffe020a4a4f1e8d42e2458fa5
casa98 <cagudeloa@uninorte.edu.co>
2020-09-18 21:05:52 -0500

    Pass needed data to ChosenCategoryFragment.kt (to know which data to fetch and display)

--- app/src/main/java/com/cagudeloa/harrypotter/CategoriesFragment.kt
+++ app/src/main/java/com/cagudeloa/harrypotter/CategoriesFragment.kt
@@ -1,15 +1,61 @@
 package com.cagudeloa.harrypotter
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_categories.*
 
 class CategoriesFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_categories, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        /**
+         * Handle clicks on each text view and navigate to the next screen
+         */
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val clickableViews: List<View> = listOf(
+            studentsText, staffText, housesText, quizText
+        )
+        for (item in clickableViews){
+            item.setOnClickListener {
+                makeColored(it)
+            }
+        }
+    }
+
+    private fun makeColored(option: View) {
+        when(option.id){
+            R.id.studentsText -> {
+                val bundle = Bundle()
+                bundle.putString("category", "student")
+                findNavController().navigate(R.id.action_categoriesFragment_to_chosenCategoryFragment, bundle)
+            }
+            R.id.staffText -> {
+                val bundle = Bundle()
+                bundle.putString("category", "staff")
+                findNavController().navigate(R.id.action_categoriesFragment_to_chosenCategoryFragment, bundle)
+            }
+            R.id.housesText -> {
+                val bundle = Bundle()
+                bundle.putString("category", "house")
+                findNavController().navigate(R.id.action_categoriesFragment_to_chosenCategoryFragment, bundle)
+            }
+            R.id.quizText -> {
+                findNavController().navigate(R.id.action_categoriesFragment_to_quizFragment)
+            }
+        }
     }
 }
--- app/src/main/java/com/cagudeloa/harrypotter/ChosenCategoryFragment.kt
+++ app/src/main/java/com/cagudeloa/harrypotter/ChosenCategoryFragment.kt
@@ -1,6 +1,7 @@
 package com.cagudeloa.harrypotter
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +9,21 @@ import android.view.ViewGroup
 
 class ChosenCategoryFragment : Fragment() {
 
+    private lateinit var category: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        /**
+         * Get arguments coming from CategoriesFragment
+         */
+        requireArguments().let {
+            category = it.getString("category")!!
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+        Log.d("Chosen", category)
         return inflater.inflate(R.layout.fragment_chosen_category, container, false)
     }
 }
